003b3d2e2d787ba3e05f1daec8f2172011f8c4ab
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2021-06-01 21:27:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log(222334);
+
+function displayemoji(){
+  document.getElementById("emoji").style.display="block";
+  document.getElementsByClassName("row text-center").innerHTML += `<div class="alert alert-warning" role="alert">
+  This is a warning alert—check it out!
+</div>`;
+
+}
